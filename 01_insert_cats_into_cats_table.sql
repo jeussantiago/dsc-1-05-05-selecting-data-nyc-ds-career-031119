967bfd8581620009b0f38e4543a0eed368d70a2d
Jeus Santiago <jeus1santiago@gmail.com>
2019-03-25 09:03:40 -0400

    DOne

--- 01_insert_cats_into_cats_table.sql
+++ 01_insert_cats_into_cats_table.sql
@@ -1,2 +1,2 @@
-cursor.execute("""INSERT INTO cats( name, age, breed) VALUES ("Lil\' Bub", 5, 'American Shorthair')""")
-cursor.execute("""INSERT INTO cats( name, age, breed) VALUES ('Hannah', 1, 'Tabby')""")
+INSERT INTO cats( name, age, breed) VALUES ("Lil' Bub", 5, 'American Shorthair')
+INSERT INTO cats( name, age, breed) VALUES ('Hannah', 1, 'Tabby')
